1f8f4584d301f52a8d6dad883b18eacafc2fd490
Lilian de Leon <deleonarleth17@gmail.com>
2023-02-25 16:05:16 -0500

    adding places view with mock data

--- views/error404.jsx
+++ views/error404.jsx
@@ -6,6 +6,7 @@ function error404 () {
       <Def>
           <main>
               <h1>404: PAGE NOT FOUND</h1>
+              <img src="http://placekitten.com/200/200" alt="kitten" />
               <p>Oops, sorry, we can't find this page!</p>
           </main>
       </Def>
